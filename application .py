07ed5e4404fba7c6c213c7fdd83e9eb48d96df49
Dromamu <70833691+Dromamu@users.noreply.github.com>
2021-07-09 22:34:36 +0530

    Update application .py

--- application .py
+++ application .py
@@ -6,7 +6,7 @@ import random
 import nltk
 import requests
 
-app = Flask(__name__, template_folder='templates')
+application = Flask(__name__, template_folder='templates')
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
 bot = ChatBot('A.U.R.A')
@@ -14,13 +14,31 @@ bot = ChatBot('A.U.R.A')
 
 trainer = ChatterBotCorpusTrainer(bot)
 trainer.train('chatterbot.corpus.english')
-@app.route("/")
+@application .route("/")
 def welcome():
     return render_template("welcome.html")
 
 
+# define app routes
+@application .route("/index/")
+def index():
+    return render_template("index.html")
+
+
+@application .route("/about_us/")
+def about_us():
+    return render_template("about_us.html")
+
+
+@application .route("/get")
+# function for the bot response
+def get_bot_response():
+        user = request.args.get("msg")
+
+        response = bot.get_response(user)
+        return str(response)
 
 
 
 if __name__ == "__main__":
-    app.run()
+    application .run()
